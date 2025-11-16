23d807cb6c498ee22a24eabb1e64f5da5e8ce961
flamboh <oliver.boorstein@gmail.com>
2025-11-15 18:39:44 -0800

    access location and song id in index

--- app/index.tsx
+++ app/index.tsx
@@ -7,17 +7,35 @@ import "../global.css";
 
 export default function Index() {
   const [showPicker, setShowPicker] = useState(false);
-
+  const [selectedSongId, setSelectedSongId] = useState<string | null>(null);
+  const [location, setLocation] = useState<{
+    latitude: number;
+    longitude: number;
+  } | null>(null);
   return (
     <View className="flex-1 bg-background">
-      <LocationMap />
+      <LocationMap onLocationChange={setLocation} />
       <View
         pointerEvents="box-none"
         className="absolute inset-x-0 bottom-12 items-center"
       >
+        {location && (
+          <View className="p-2">
+            <Text className="text-muted-foreground">
+              Location: {location.latitude}, {location.longitude}
+            </Text>
+          </View>
+        )}
+        {selectedSongId && (
+          <View className="p-2">
+            <Text className="text-muted-foreground">
+              Selected song: {selectedSongId}
+            </Text>
+          </View>
+        )}
         {showPicker && (
           <View className="my-2 w-min max-h-[24rem] rounded-2xl border border-border bg-background/95 shadow-lg">
-            <SongPicker />
+            <SongPicker onSongSelected={setSelectedSongId} />
           </View>
         )}
         <Pressable
