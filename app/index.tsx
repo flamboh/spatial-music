3cd61fb68da2692bfa9a7a39b04022c7ae22c1c3
Kobe Pane <kobepane@gmail.com>
2025-11-15 16:51:56 -0800

    hope

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,6 @@
-import { useQuery } from "convex/react";
-import { Text, View } from "react-native";
-import { api } from "../convex/_generated/api";
+import LocationMap from "../components/LocationMap";
+import "../global.css";
 
 export default function Index() {
-  const tasks = useQuery(api.tasks.get);
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      {tasks?.map((task) => (
-        <Text key={task._id}>{task.text}</Text>
-      ))}
-    </View>
-  );
+  return <LocationMap />;
 }
